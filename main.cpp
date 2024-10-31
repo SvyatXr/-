3fb59a2309e3b72a28e1f586fdec6a2bf9126307
SvyatXr <svyat2306@gmail.com>
2024-10-31 10:30:20 +0400

    Луна и дым из трубы

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "TXLib.h"
 
-void drawSky()
+void drawSky(COLORREF color)
 {
+//TX_LIGHTBLUE
     txSetColor (TX_LIGHTBLUE);
-    txSetFillColor (TX_LIGHTBLUE);
+    txSetFillColor (color);
     txRectangle (0, 0, 800, 600);
 }
 
@@ -11,7 +12,7 @@ void drawSun(int x)
 {
     txSetColor (TX_YELLOW);
     txSetFillColor (TX_YELLOW);
-    txCircle (x, 100, 50);
+    txCircle (x, 100, 70);
 }
 
 void drawCloud(int x)
@@ -21,8 +22,9 @@ void drawCloud(int x)
     txEllipse(x+445-320, 70, x+745-320, 160);
     txEllipse(x+320-320, 70, x+570-320, 170);
     txEllipse(x+400-320, 20, x+675-320, 130);
-}
+    txCircle(410, 160, 10);
 
+}
 void drawMan()
 {
     txSetColor (TX_BLACK);
@@ -55,6 +57,9 @@ void drawhouse()
     //скат крыши
     POINT krysha2[4] = {{220, 220}, {360, 360}, {600, 200}, {460, 110}};
     txPolygon (krysha2, 4);
+    //труба
+    txSetFillColor (TX_BROWN);
+    txRectangle (390, 170, 430, 280);
     //окно
     txSetFillColor (TX_YELLOW);
     txRectangle (170, 410, 280, 520);
@@ -68,6 +73,22 @@ void drawTrava()
 
 }
 
+
+void drawLuna(int x)
+{
+    txSetColor (TX_LIGHTGRAY);
+    txSetFillColor (TX_LIGHTGRAY);
+    txCircle (x, 150, 50);
+}
+
+void drawSmoke(int x, int y)
+{
+    txSetColor (TX_WHITE);
+    txSetFillColor (TX_WHITE);
+    txEllipse(x+380-380, y+140-140, x+430-380, y+170-140);
+}
+
+
 int main()
 {
 
@@ -75,26 +96,45 @@ txCreateWindow (800, 600);
 
     int xSun = 100;
     int xCloud = 320;
+    int xLuna = -150;
+    int xSmoke = 380;
+    int ySmoke = 140;
 
 
     while(xSun<1000)
     {
-
-        drawSky();
+        txBegin();
+        drawSky(TX_LIGHTBLUE);
         drawSun(xSun);
         drawCloud(xCloud);
         drawTrava();
         drawhouse();
+
+        drawSmoke(xSmoke, ySmoke);
+
         drawMan();
+        txEnd();
 
         xSun += 5;
         xCloud -= 10;
+        xSmoke -= 2;
+        ySmoke -= 1;
         txSleep(10);
 
     }
+    while(xLuna<1000)
+    {
+        txBegin();
+        drawSky(TX_BLACK);
+        drawLuna(xLuna);
+        drawTrava();
+        drawhouse();
+        drawMan();
+        txEnd();
 
-
-
+        xLuna += 5;
+        txSleep(10);
+    }
 
 txTextCursor (false);
 return 0;
